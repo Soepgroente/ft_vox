605e97c1de5e0de424898f48d0dd97d7c69734ac
Francesco <francesco1896@gmail.com>
2026-03-13 03:09:58 +0100

    texture applied on voxels ok

--- include/KeyboardInput.hpp
+++ include/KeyboardInput.hpp
@@ -2,12 +2,8 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#include <vector>
 #include <array>
-#include <functional>
 
-#include "Vulkan.hpp"
 
 namespace vox {
 
--- include/MouseInput.hpp
+++ include/MouseInput.hpp
@@ -1,12 +1,8 @@
 #pragma once
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
-#include <vector>
 #include <array>
-#include <cmath>
-
-#include "Vulkan.hpp"
 
 
 namespace vox {
