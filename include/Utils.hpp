8abea492bb91e6784d48fc50e1aec0e2733010ec
Francesco <francesco1896@gmail.com>
2026-03-13 04:10:11 +0100

    display current GPU memory used

---
+++ include/Utils.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+
+namespace vox {
+
+std::string formatBytes( size_t );
+
+}	// namespace vox
--- source/Utils.cpp
+++ source/Utils.cpp
@@ -1,4 +1,29 @@
+#include "Utils.hpp"
+
+#include <sstream>
+#include <iomanip>
+
+
 namespace vox {
 
+std::string formatBytes( size_t bytes ) {
+	constexpr size_t KB = 1024UL;
+	constexpr size_t MB = 1024UL * KB;
+	constexpr size_t GB = 1024UL * MB;
+
+	std::ostringstream oss;
+	oss << std::fixed << std::setprecision(3);
+
+	if (bytes >= GB)
+		oss << static_cast<double>(bytes) / GB << " GiB";
+	else if (bytes >= MB)
+		oss << static_cast<double>(bytes) / MB << " MiB";
+	else if (bytes >= KB)
+		oss << static_cast<double>(bytes) / KB << " KiB";
+	else
+		oss << bytes << " B";
+
+	return oss.str();
+}
 
 }	// namespace vox
