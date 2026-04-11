ca46cdba84d55c5c525e7ea1ad9ca7fbc2af6794
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2026-04-11 22:31:37 +0200

    Improve vertex generation (preparation for face culling)

--- include/World.hpp
+++ include/World.hpp
@@ -19,23 +19,23 @@ inline constexpr uint32_t	INDEX_PER_VOXEL = 36U;	// number of vertex indexes per
 inline constexpr float		VOXEL_SIZE = 1.0f;		// length of a voxel edge
 
 /*
- * 1 voxel corresponds to:
- * vertexes = 24 (vertexes per voxel) * 32 (bytes per vertex) = 768 b
- * indexes = 36 (indexes per voxel) * 4 (bytes per index [uint]) = 144 b
- * total = 912 b
- *
- * 32 * 32 voxels correspond to:
- * vertexes = 32 * 32 * 768 b = 786432 b = 768 KiB
- * indexes = 32 * 32 * 144 b = 147456 b = 144 KiB
- * total = 933888 b = 912 KiB
- *
- * an arbitrary limit of 114 MiB, means 128 (worlds) * 32 * 32 (voxels per chunk) = 2^17 voxels
- * vertexes = 128 * 32 * 32 * 768 b = 100663296 b = 98304 KiB = 96 MiB
- * indexes = 128 * 32 * 32 * 144 b = 18874368 b = 18432 KiB = 18 MiB
- * total = 119537664 b = 116736 KiB = 114 MiB
- * 
- * @note this will change since the size of a world will not always be 32 * 32 voxels
- */
+* 1 voxel corresponds to:
+* vertexes = 24 (vertexes per voxel) * 32 (bytes per vertex) = 768 b
+* indexes = 36 (indexes per voxel) * 4 (bytes per index [uint]) = 144 b
+* total = 912 b
+*
+* 32 * 32 voxels correspond to:
+* vertexes = 32 * 32 * 768 b = 786432 b = 768 KiB
+* indexes = 32 * 32 * 144 b = 147456 b = 144 KiB
+* total = 933888 b = 912 KiB
+*
+* an arbitrary limit of 114 MiB, means 128 (worlds) * 32 * 32 (voxels per chunk) = 2^17 voxels
+* vertexes = 128 * 32 * 32 * 768 b = 100663296 b = 98304 KiB = 96 MiB
+* indexes = 128 * 32 * 32 * 144 b = 18874368 b = 18432 KiB = 18 MiB
+* total = 119537664 b = 116736 KiB = 114 MiB
+* 
+* @note this will change since the size of a world will not always be 32 * 32 voxels
+*/
 inline constexpr uint32_t	MAX_WORLDS = 128U;
 
 enum class VertexFaces : size_t
@@ -48,38 +48,42 @@ enum class VertexFaces : size_t
 	BOTTOM = 20
 };
 
+static constexpr float W = 1.0f / 4.0f;  // width of a tile
+static constexpr float H = 1.0f / 3.0f;  // height of a tile
+static constexpr float padding = 0.004f;
+
 // Hard-coded VBO (vertex+normal+textureUV data) of a voxel
 inline constexpr std::array<ve::VulkanModel::Vertex,VERTEX_PER_VOXEL> VOXEL_VERTEXES{
 	// face FRONT (z = +0.5)
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3::backward(), vec2{ W + padding, 3 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3::backward(), vec2{ 2 * W - padding, 3 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3::backward(), vec2{ 2 * W - padding, 2 * H + padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3::backward(), vec2{ W + padding, 2 * H + padding }},
 	//face BACK (z = -0.5)
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3::forward(), vec2{ 2 * W - padding, padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3::forward(), vec2{ W + padding, padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3::forward(), vec2{ W + padding, H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3::forward(), vec2{ 2 * W - padding, H - padding }},
 	// face LEFT (x = -0.5)
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3::left(), vec2{ padding, H + padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3::left(), vec2{ padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3::left(), vec2{ W - padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3::left(), vec2{ W - padding, H + padding }},
 	// face RIGHT (x = +0.5)
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3::right(), vec2{ 3 * W - padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3::right(), vec2{ 3 * W - padding, H + padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3::right(), vec2{ 2 * W + padding, H + padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3::right(), vec2{ 2 * W + padding, 2 * H - padding }},
 	// face TOP (y = +0.5)
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f,  0.5f }, vec3::up(), vec2{ W + padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f,  0.5f }, vec3::up(), vec2{ 2 * W - padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f,  0.5f, -0.5f }, vec3::up(), vec2{ 2 * W - padding, H + padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f,  0.5f, -0.5f }, vec3::up(), vec2{ W + padding, H + padding }},
 	// face BOTTOM (y = -0.5)
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 0.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3(0.0f), vec2{ 1.0f, 1.0f }},
-	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 1.0f, 0.0f }},
-	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3(0.0f), vec2{ 0.0f, 0.0f }}
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f, -0.5f }, vec3::down(), vec2{ 3 * W + padding, H + padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f, -0.5f }, vec3::down(), vec2{ 3 * W + padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{  0.5f, -0.5f,  0.5f }, vec3::down(), vec2{ 4 * W - padding, 2 * H - padding }},
+	ve::VulkanModel::Vertex{vec3{ -0.5f, -0.5f,  0.5f }, vec3::down(), vec2{ 4 * W - padding, H + padding }}
 };
 
 // hard-coded face indexes of a voxel
@@ -101,7 +105,7 @@ inline constexpr std::array<uint32_t, INDEX_PER_VOXEL> VOXEL_VERTEX_INDEXES{
 using VertexVector = std::vector<ve::VulkanModel::Vertex>;
 using IndexVector = std::vector<uint32_t>;
 
-void	getVertexRelativeMonoTexture( vec3 const& position, VertexVector& chunk );
+void	addVoxelFace(const vec3& voxelLocation, VertexVector& chunk, size_t faceIndex);
 void	getVertexRelativeAtlasTexture( vec3 const& position, VertexVector& chunk);
 IndexVector		getIndexRelative( uint32_t = 0U );
 
--- source/World.cpp
+++ source/World.cpp
@@ -16,26 +16,28 @@ namespace vox {
  * Get the vertex+texture+normal coordinates of a voxel. The texture coordinates are supposed to
  * apply a whole texture on every face of the voxel
  *
- * @param relativeOrigin The origin of the voxel in a 3D space, the coordinates are relative
+ * @param voxelLocation The origin of the voxel in a 3D space, the coordinates are relative
  * from such position (default is (0.0f, 0.0f, 0.0f))
  *
  * @return a vector of 24 (fixed number of vertexes per voxel) 
  * instances of ve::VulkanModel::Vertex
  */
-void	getVertexRelativeMonoTexture(const vec3& relativeOrigin, VertexVector& chunk)
+
+void	addVoxelFace(const vec3& location, VertexVector& chunk, size_t min)
 {
-	for (ui32 i = 0; i < VERTEX_PER_VOXEL; i++)
+	size_t max = min + 4;
+
+	for (size_t i = min; i < max; i++)
 	{
-		// add 0.5 (half size of a voxel) of every coor so that the position is in the exact center
 		chunk.emplace_back
 		(
 			ve::VulkanModel::Vertex
 			{
 				vec3
 				{
-					VOXEL_VERTEXES[i].pos.x + VOXEL_SIZE * 0.5f + relativeOrigin.x,
-					VOXEL_VERTEXES[i].pos.y + VOXEL_SIZE * 0.5f + relativeOrigin.y,
-					VOXEL_VERTEXES[i].pos.z + VOXEL_SIZE * 0.5f + relativeOrigin.z
+					VOXEL_VERTEXES[i].pos.x + VOXEL_SIZE * 0.5f + location.x,
+					VOXEL_VERTEXES[i].pos.y + VOXEL_SIZE * 0.5f + location.y,
+					VOXEL_VERTEXES[i].pos.z + VOXEL_SIZE * 0.5f + location.z
 				},
 			VOXEL_VERTEXES[i].normal,
 			VOXEL_VERTEXES[i].textureUv
@@ -73,46 +75,14 @@ std::vector<vec3> getVertexRelative( vec3 const& relativeOrigin ) {
  * @return a vector of 24 (fixed number of vertexes per voxel) 
  * instances of ve::VulkanModel::Vertex
  */
-void	getVertexRelativeAtlasTexture(const vec3& relativeOrigin, VertexVector& chunk)
+void	getVertexRelativeAtlasTexture(const vec3& voxelLocation, VertexVector& chunk)
 {
-	size_t index = chunk.size();
-
-	getVertexRelativeMonoTexture(relativeOrigin, chunk);
-
-	static constexpr float W = 1.0f / 4.0f;  // width of a tile
-	static constexpr float H = 1.0f / 3.0f;  // height of a tile
-	static constexpr float padding = 0.004f;
-
-	// front face
-	chunk[index + 0].textureUv = vec2{W + padding, 3 * H - padding};
-	chunk[index + 0 + 1].textureUv = vec2{2 * W - padding, 3 * H - padding};
-	chunk[index + 0 + 2].textureUv = vec2{2 * W - padding, 2 * H + padding};
-	chunk[index + 0 + 3].textureUv = vec2{W + padding, 2 * H + padding};
-	// back face
-	chunk[index + 4].textureUv = vec2{2 * W - padding, padding};
-	chunk[index + 4 + 1].textureUv = vec2{W + padding, padding};
-	chunk[index + 4 + 2].textureUv = vec2{W + padding, H - padding};
-	chunk[index + 4 + 3].textureUv = vec2{2 * W - padding, H - padding};
-	// left face
-	chunk[index + 8].textureUv = vec2{padding, H + padding};
-	chunk[index + 8 + 1].textureUv = vec2{padding, 2 * H - padding};
-	chunk[index + 8 + 2].textureUv = vec2{W - padding, 2 * H - padding};
-	chunk[index + 8 + 3].textureUv = vec2{W - padding, H + padding};
-	// right face
-	chunk[index + 12].textureUv = vec2{3 * W - padding, 2 * H - padding};
-	chunk[index + 12 + 1].textureUv = vec2{3 * W - padding, H + padding};
-	chunk[index + 12 + 2].textureUv = vec2{2 * W + padding, H + padding};
-	chunk[index + 12 + 3].textureUv = vec2{2 * W + padding, 2 * H - padding};
-	// top face
-	chunk[index + 16].textureUv = vec2{W + padding, 2 * H - padding};
-	chunk[index + 16 + 1].textureUv = vec2{2 * W - padding, 2 * H - padding};
-	chunk[index + 16 + 2].textureUv = vec2{2 * W - padding, H + padding};
-	chunk[index + 16 + 3].textureUv = vec2{W + padding, H + padding};
-	// back face
-	chunk[index + 20].textureUv = vec2{3 * W + padding, H + padding};
-	chunk[index + 20 + 1].textureUv = vec2{3 * W + padding, 2 * H - padding};
-	chunk[index + 20 + 2].textureUv = vec2{4 * W - padding, 2 * H - padding};
-	chunk[index + 20 + 3].textureUv = vec2{4 * W - padding, H + padding};
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::FRONT));
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::BACK));
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::LEFT));
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::RIGHT));
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::TOP));
+	addVoxelFace(voxelLocation, chunk, static_cast<size_t>(VertexFaces::BOTTOM));
 }
 
 /**
